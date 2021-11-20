99446069aa18e0a12126f12105291c3fdee990f2
itdev.support07 <itdev.support07@tokiomarine-life.co.id>
2021-11-20 23:10:32 +0700

    Initial Commit

---
+++ MyGames/MyGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGamesApp.swift
+//  MyGames
+//
+//  Created by TMLI IT Dev on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MyGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
